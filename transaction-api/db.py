fa56ded30a6d47e9bad79e9abeadf1b0eaa59d2a
adhfmz8 <73725651+adhfmz8@users.noreply.github.com>
2024-11-03 00:42:14 -0600

    get and post endpoints

--- transaction-api/db.py
+++ transaction-api/db.py
@@ -5,10 +5,10 @@ DATABASE_URL = "sqlite:///db.sqlite"
 engine = create_engine(DATABASE_URL, echo=True)
 
 
-def initdb():
+def init_db():
     SQLModel.metadata.create_all(engine)
 
 
-async def getsession():
+async def get_session():
     with Session(engine) as session:
         yield session
--- transaction-api/main.py
+++ transaction-api/main.py
@@ -1,8 +1,35 @@
-from fastapi import FastAPI, Response
+from fastapi import FastAPI, Response, Depends
+from sqlmodel import Session, select
+from contextlib import asynccontextmanager
+from models import Transaction
+from db import init_db, get_session
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(applet: FastAPI):
+    init_db()
+    yield
 
 
-@app.get("/")
-async def getRoot():
-    return Response(content="Hello from FastAPI", media_type="text/plain")
+app = FastAPI(lifespan=lifespan)
+
+
+@app.get("/api/v1/transaction")
+async def get_all_transactions(
+    session: Session = Depends(get_session),
+) -> list[Transaction]:
+    transactions = session.exec(select(Transaction)).all()
+    return list(transactions)
+
+
+@app.post("/api/v1/transaction")
+async def post_transaction(
+    t: Transaction, session: Session = Depends(get_session)
+) -> Transaction:
+    transaction = Transaction(name=t.name, cost=t.cost, frequency=t.frequency)
+
+    session.add(transaction)
+    session.commit()
+    session.refresh(transaction)
+
+    return transaction
