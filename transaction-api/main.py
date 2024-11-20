7fb6f475aef038a351da7eaf8b839c5a2d9ff61a
adhfmz8 <73725651+adhfmz8@users.noreply.github.com>
2024-11-20 09:35:18 -0700

    Added put and get 1 transaction

--- transaction-api/main.py
+++ transaction-api/main.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI, Response, Depends
+from typing import Type, Sequence
+
+from fastapi import FastAPI, Response, Depends, HTTPException
 from sqlmodel import Session, select
 from contextlib import asynccontextmanager
-from models import Transaction
+from models import Transaction, EditTransaction
 from db import init_db, get_session
 
 
@@ -17,16 +19,51 @@ app = FastAPI(lifespan=lifespan)
 @app.get("/api/v1/transaction")
 async def get_all_transactions(
     session: Session = Depends(get_session),
-) -> list[Transaction]:
+) -> Sequence[Transaction]:
     transactions = session.exec(select(Transaction)).all()
-    return list(transactions)
+    return transactions
 
 
 @app.post("/api/v1/transaction")
 async def post_transaction(
     t: Transaction, session: Session = Depends(get_session)
 ) -> Transaction:
-    transaction = Transaction(name=t.name, cost=t.cost, frequency=t.frequency)
+    transaction = Transaction(name=t.name, cost=t.cost)
+
+    session.add(transaction)
+    session.commit()
+    session.refresh(transaction)
+
+    return transaction
+
+
+@app.get("/api/v1/transaction/{id_num}")
+async def get_transaction(id_num: int, session: Session = Depends(get_session)):
+
+    transaction = session.get(Transaction, id_num)
+
+    if not transaction:
+        raise HTTPException(status_code=404, detail=f"Transaction with id {id_num} not found")
+
+    return transaction
+
+
+@app.put("/api/v1/transaction/{id_num}")
+async def update_transaction(
+        t: EditTransaction,
+        id_num: int,
+        session: Session = Depends(get_session)
+):
+
+    transaction: Type[Transaction] = session.get(Transaction, id_num)
+
+    if not transaction:
+        raise HTTPException(status_code=404, detail=f"Transaction with id {id_num} not found")
+
+    if t.name is not None:
+        transaction.name = t.name
+    if t.cost is not None:
+        transaction.cost = t.cost
 
     session.add(transaction)
     session.commit()
--- transaction-api/tests.py
+++ transaction-api/tests.py
@@ -5,6 +5,5 @@ client = TestClient(app)
 
 
 def test_main():
-    response = client.get("/")
+    response = client.get("/api/v1/transaction")
     assert response.status_code == 200
-    assert response.text == "Hello from FastAPI"
